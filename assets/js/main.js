ecca8722cb714fdc86a984f85a314754a2f107dc
arundas <arun.das@webeaz.com>
2023-05-20 16:41:50 +0530

    sub

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,64 @@ $('.menu-toggle').click(function() {
     $(this).toggleClass('open');
 
   })
+  // Service and team carousel
+  $(".service-carousel, .team-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="fa-solid fa-chevron-left" aria-hidden="true"></i>',
+        '<i class="fa-solid fa-chevron-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+$(".banner-carowsel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1500,
+  margin: 30,
+  dots: true,
+  loop: true,
+  nav : false,
+  navText : [
+      '<i class="fa-solid fa-chevron-left" aria-hidden="true"></i>',
+      '<i class="fa-solid fa-chevron-right" aria-hidden="true"></i>'
+  ],
+  responsive: {
+      0:{
+          items:1
+      },
+      576:{
+          items:1
+      },
+      768:{
+          items:1
+      },
+      992:{
+          items:1
+      }
+  }
+});
+// Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 1500,
+  dots: true,
+  loop: true,
+  items: 1
+});
