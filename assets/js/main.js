7c60e2838b5206fc507e781e7441ce44556f4adf
rashidamahroof <rashidamahroofp@gmail.com>
2023-05-22 07:52:11 +0530

    oik

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,12 @@ $(".testimonial-carousel").owlCarousel({
   loop: true,
   items: 1
 });
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
