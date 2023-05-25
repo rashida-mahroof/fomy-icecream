e513e26d212039ff053245710b24d4dd50a43319
rashidamahroof <rashidamahroofp@gmail.com>
2023-05-25 23:25:44 +0530

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 $('.menu-toggle').click(function() {
 
     /*--------------------------------------------------------------*/
@@ -72,12 +73,3 @@ $(".testimonial-carousel").owlCarousel({
   loop: true,
   items: 1
 });
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
